4a26372947161aceff91ad3f6d12e1a055b8cdb8
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-09-23 17:07:19 +0300

    :page_facing_up: Belgeleme sistemi oluşturuldu.
    resolves #2

---
+++ data/Navigation.js
@@ -0,0 +1,72 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'MEZBELE',
+		text: 'MEZBELE',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/MEZBELE' },
+		children: [
+			
+ 
+
+		{
+			id: 'MEZBELE.BundleConfig',
+			text: 'BundleConfig',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.BundleConfig/index' }
+        },	
+		{
+			id: 'MEZBELE.FilterConfig',
+			text: 'FilterConfig',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.FilterConfig/index' }
+        },	
+		{
+			id: 'MEZBELE.MvcApplication',
+			text: 'MvcApplication',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.MvcApplication/index' }
+        },	
+		{
+			id: 'MEZBELE.RouteConfig',
+			text: 'RouteConfig',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.RouteConfig/index' }
+        },	
+		{
+			id: 'MEZBELE.WebApiConfig',
+			text: 'WebApiConfig',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.WebApiConfig/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'MEZBELE.Controllers',
+		text: 'MEZBELE.Controllers',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/MEZBELE.Controllers' },
+		children: [
+			
+ 
+
+		{
+			id: 'MEZBELE.Controllers.HomeController',
+			text: 'HomeController',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Controllers.HomeController/index' }
+        },	
+
+
+		]
+	},
+
+
+	];
---
+++ data/Project.js
@@ -0,0 +1,12 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+  name: 'Mezbele',
+   homepage: 'https://github.com/nuriu/mezbele',    version: '0.1',    author: 'Nuri Uzunoğlu',    authorHomepage: 'http://nuriuzunoglu.com',     showCode: false,
+    hasLogo: true,
+  targetFxs: [
+			"Unknown",
+	  ]
+}
