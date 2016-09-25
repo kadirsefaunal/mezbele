057bfdc3292f2a061434f3aae407ed5887a43e58
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-09-25 21:33:45 +0300

    Belgeler güncellendi.

--- data/Navigation.js
+++ data/Navigation.js
@@ -49,6 +49,25 @@ sharpDox.navigationData = [
 		]
 	},
 	{
+		id: 'MEZBELE.Context',
+		text: 'MEZBELE.Context',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/MEZBELE.Context' },
+		children: [
+			
+ 
+
+		{
+			id: 'MEZBELE.Context.MezbeleContext',
+			text: 'MezbeleContext',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Context.MezbeleContext/index' }
+        },	
+
+
+		]
+	},
+	{
 		id: 'MEZBELE.Controllers',
 		text: 'MEZBELE.Controllers',
 		icon: './assets/icons/namespace_public.png',
@@ -62,6 +81,92 @@ sharpDox.navigationData = [
 			text: 'LandingController',
 			icon: './assets/icons/class_public.png',
 			a_attr: { href : '#/type/MEZBELE.Controllers.LandingController/index' }
+        },	
+		{
+			id: 'MEZBELE.Controllers.UsersController',
+			text: 'UsersController',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Controllers.UsersController/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'MEZBELE.Migrations',
+		text: 'MEZBELE.Migrations',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/MEZBELE.Migrations' },
+		children: [
+			
+ 
+
+		{
+			id: 'MEZBELE.Migrations.AddedUserPassword',
+			text: 'AddedUserPassword',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Migrations.AddedUserPassword/index' }
+        },	
+		{
+			id: 'MEZBELE.Migrations.AddUserPassword',
+			text: 'AddUserPassword',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Migrations.AddUserPassword/index' }
+        },	
+		{
+			id: 'MEZBELE.Migrations.Configuration',
+			text: 'Configuration',
+			icon: './assets/icons/class_ınternal.png',
+			a_attr: { href : '#/type/MEZBELE.Migrations.Configuration/index' }
+        },	
+		{
+			id: 'MEZBELE.Migrations.RemovedUserPassword',
+			text: 'RemovedUserPassword',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Migrations.RemovedUserPassword/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'MEZBELE.Models',
+		text: 'MEZBELE.Models',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/MEZBELE.Models' },
+		children: [
+			
+ 
+
+		{
+			id: 'MEZBELE.Models.Comments',
+			text: 'Comments',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Models.Comments/index' }
+        },	
+		{
+			id: 'MEZBELE.Models.Crews',
+			text: 'Crews',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Models.Crews/index' }
+        },	
+		{
+			id: 'MEZBELE.Models.Issues',
+			text: 'Issues',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Models.Issues/index' }
+        },	
+		{
+			id: 'MEZBELE.Models.Projects',
+			text: 'Projects',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Models.Projects/index' }
+        },	
+		{
+			id: 'MEZBELE.Models.Users',
+			text: 'Users',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/MEZBELE.Models.Users/index' }
         },	
 
 
