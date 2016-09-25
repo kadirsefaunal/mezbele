136a078a101be951747058dc9c7c63e7626a0759
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-09-25 14:04:36 +0300

    :closed_lock_with_key: Kullanıcı kayıt arayüzü oluşturuldu.
    resolves #10

--- MEZBELE/Scripts/site.js
+++ MEZBELE/Scripts/site.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function () {
+    // fixed menu
     $('.masthead').visibility({
         once: false,
         onBottomPassed: function () {
@@ -9,5 +10,51 @@
         }
     });
 
+    // sidebar
     $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+    // kayıt form
+    $('.ui.form').form({
+        fields: {
+            username: {
+                identifier: 'username',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Lütfen kullanıcı adınızı girin.'
+                    }
+                ]
+            },
+            email: {
+                identifier: 'email',
+                rules: [
+                  {
+                      type: 'empty',
+                      prompt: 'Lütfen e-posta adresi girin.'
+                  },
+                  {
+                      type: 'email',
+                      prompt: 'Lütfen geçerli bir e-posta adresi giriniz.'
+                  }
+                ]
+            },
+            password: {
+                identifier: 'password',
+                rules: [
+                  {
+                      type: 'empty',
+                      prompt: 'Lütfen parolanızı girin.'
+                  },
+                  {
+                      type: 'length[6]',
+                      prompt: 'Parolanız en az 6 haneli olmalıdır.'
+                  }
+                ]
+            }
+        }
+    });
 });
+
+function showSignUpModal() {
+    $('#signUpModal').modal('show');
+}
