6c0b60e3c2670adf21ef02fa719268c2450c2d0a
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-09-26 14:32:48 +0300

    :tv: Uygulama baz arayüzü oluşturuldu.
    resolves #13

---
+++ MEZBELE/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MEZBELE.Controllers
+{
+    /// <summary>
+    /// Ana uygulama kontrolcüsü.
+    /// </summary>
+    public class AppController : Controller
+    {
+        /// <summary>
+        /// Uygulama anasayfasının kontrolcüsü.
+        /// </summary>
+        /// <returns>Uygulama anasayfasını döndürür.</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
