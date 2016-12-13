215d9355ec15b789b8e8ef24d5e0f95333dbdb8e
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-12-13 19:45:00 +0300

    Kullanıcı bilgilerini değiştirme özelliği tamamlandı.

--- MEZBELE/Controllers/AppController.cs
+++ MEZBELE/Controllers/AppController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Linq;
 using System;
+using System.Data.Entity;
 
 namespace MEZBELE.Controllers
 {
@@ -25,8 +26,8 @@ namespace MEZBELE.Controllers
             {
                 int kullaniciID = int.Parse(Request.Cookies["KullaniciKimligi"].Value);
                 ViewBag.Kullanici = (from k in db.Users
-                                 where k.ID == kullaniciID
-                                 select k).SingleOrDefault();
+                                     where k.ID == kullaniciID
+                                     select k).SingleOrDefault();
                 return View();
             }
             return RedirectToAction("Index", "Landing");
@@ -42,11 +43,49 @@ namespace MEZBELE.Controllers
             {
                 int kullaniciID = int.Parse(Request.Cookies["KullaniciKimligi"].Value);
                 ViewBag.Kullanici = (from k in db.Users
-                                 where k.ID == kullaniciID
-                                 select k).SingleOrDefault();
+                                     where k.ID == kullaniciID
+                                     select k).SingleOrDefault();
                 return View();
             }
             return RedirectToAction("Index", "Landing");
+        }
+
+        public JsonResult UpdateUserInfo(string isim, string soyisim, string eposta,
+                                         string parola, string webLink, string konum,
+                                         string bolge, string avatarLink)
+        {
+            if (Request.Cookies["KullaniciKimligi"] != null)
+            {
+                int kullaniciID = int.Parse(Request.Cookies["KullaniciKimligi"].Value);
+                var kullanici = (from k in db.Users where k.ID == kullaniciID select k).SingleOrDefault();
+
+                if (kullanici != null)
+                {
+                    kullanici.FirstName = isim;
+                    kullanici.LastName = soyisim;
+                    kullanici.EMail = eposta;
+                    kullanici.Password = parola;
+                    kullanici.WebAdresi = webLink;
+                    kullanici.Konum = konum;
+                    kullanici.Bolge = bolge;
+                    kullanici.UserAvatar = avatarLink;
+
+                    var girdi = db.Entry(kullanici);
+
+                    girdi.State = EntityState.Modified;
+                    girdi.Property(k => k.UserName).IsModified = false;
+                    girdi.Property(k => k.RoleID).IsModified = false;
+                    girdi.Property(k => k.SonGiris).IsModified = false;
+
+                    db.SaveChanges();
+
+                    return Json("Güncelleme başarılı.");
+                }
+
+                return Json("Güncelleme başarısız.");
+            }
+
+            return Json("Güncelleme başarısız.");
         }
 
         protected override void Dispose(bool disposing)
