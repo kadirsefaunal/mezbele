06ca0c9542051bdae6e95693236bf658c9f00a95
kadirsefaunal <kadir-unal@outlook.com>
2016-12-16 21:08:26 +0300

    Proje listeleme deneme kodları eklendi.

--- MEZBELE/ViewModels/VM.cs
+++ MEZBELE/ViewModels/VM.cs
@@ -1,9 +1,11 @@
 ﻿using MEZBELE.Models;
+using System.Collections.Generic;
 
 namespace MEZBELE.ViewModels
 {
     public class VM
     {
         public User Kullanici { get; set; }
+        public List<Project> Projeler { get; set; }
     }
 }
