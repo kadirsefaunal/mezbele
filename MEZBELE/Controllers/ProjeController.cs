5af00bc1449e1c8c641e15f6233c2108098b4a79
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-12-24 01:29:46 +0300

    Proje detay sayfası baz arayüzü eklendi. Bağlantılar sağlandı.

---
+++ MEZBELE/Controllers/ProjeController.cs
@@ -0,0 +1,52 @@
+﻿using MEZBELE.Models;
+using System.Web.Mvc;
+using System.Linq;
+using System.Data.Entity;
+using MEZBELE.ViewModels;
+using System;
+using System.Net;
+
+namespace MEZBELE.Controllers
+{
+    public class ProjeController : Controller
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        private readonly MezbeleDBEntities db = new MezbeleDBEntities();
+        /// <summary>
+        ///
+        /// </summary>
+        private VM vm;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="projeKimligi"></param>
+        /// <returns></returns>
+        public ActionResult Detay(int projeKimligi)
+        {
+            if (Request.Cookies["KullaniciKimligi"] != null)
+            {
+                if (projeKimligi == 0)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                int kullaniciID = int.Parse(Request.Cookies["KullaniciKimligi"].Value);
+
+                vm = new VM();
+
+                vm.Kullanici = (from k in db.Kullanici
+                                where k.ID == kullaniciID
+                                select k).SingleOrDefault();
+
+                vm.AktifProje = (from p in db.Proje
+                                 where p.ID == projeKimligi
+                                 select p).SingleOrDefault();
+                return View(vm);
+            }
+            return RedirectToAction("Index", "Landing");
+        }
+    }
+}
--- MEZBELE/ViewModels/VM.cs
+++ MEZBELE/ViewModels/VM.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 
 namespace MEZBELE.ViewModels
 {
+    /// <summary>
+    ///
+    /// </summary>
     public class VM
     {
+        /// <summary>
+        ///
+        /// </summary>
         public Kullanici Kullanici { get; set; }
+        /// <summary>
+        ///
+        /// </summary>
         public List<Proje> Projeler { get; set; }
+        /// <summary>
+        ///
+        /// </summary>
+        public Proje AktifProje { get; set; }
     }
 }
