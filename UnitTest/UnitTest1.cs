f21afc32b1d62c8296d428739e383d2cff8b87b7
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-12-27 17:32:13 +0300

    Testler eklendi.

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MEZBELE.Controllers;
+using System.Web.Mvc;
 
 namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        ///
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void LoginTest()
         {
+            LandingController lc = new LandingController();
+            var a = lc.LoginControl("321", "321") as JsonResult;
+            var b = lc.LoginControl(null, null) as JsonResult;
+            Assert.AreEqual(b.Data, a.Data);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [TestMethod]
+        public void ProjeKaydetTest()
+        {
+            AppController ac = new AppController();
+            var a = ac.ProjeKaydet(null) as JsonResult;
+            var b = ac.ProjeKaydet(new MEZBELE.Models.Proje()) as JsonResult;
+            Assert.AreEqual(b.Data, a.Data);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [TestMethod]
+        public void IsEkleTest()
+        {
+            ProjeController pc = new ProjeController();
+            var a = pc.IsEkle(null) as JsonResult;
+            var b = pc.IsEkle(new MEZBELE.Models.Is()) as JsonResult;
+            Assert.AreEqual(b.Data, a.Data);
         }
     }
 }
